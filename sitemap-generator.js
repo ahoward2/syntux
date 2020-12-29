713645e9025f3ed5b2d6d2dbf8fcacd4a791939f
Austin Howard <ahoward2@unca.edu>
2020-12-29 17:11:16 -0500

    feat: add sitemap generator script that runs postbuild

---
+++ sitemap-generator.js
@@ -0,0 +1,12 @@
+const sitemap = require("nextjs-sitemap-generator");
+const fs = require("fs");
+
+const BUILD_ID = fs.readFileSync(".next/BUILD_ID").toString();
+
+sitemap({
+  baseUrl: "https://icycoder.com",
+  pagesDirectory: __dirname + "/.next/server/pages",
+  targetDirectory: "public/",
+  ignoredExtensions: ["js", "map"],
+  ignoredPaths: ["[fallback]"],
+});
