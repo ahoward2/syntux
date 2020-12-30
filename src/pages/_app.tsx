70993e0bcc365d5d197a5abd1bf354f9c96e7a45
Austin Howard <ahoward2@unca.edu>
2020-12-29 20:22:11 -0500

    feat: install and implement GA for pageviews

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,19 @@ import { AppProps } from "next/app";
 // NOTE: Do not move the styles dir to the src.
 // They are used by the Netlify CMS preview feature.
 import "../../public/styles/global.css";
+import {initGA} from "../lib/analytics";
+import { useEffect, useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+
+  const [analytics, setAnalytics] = useState(false);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined' && !analytics) {
+      initGA();
+      setAnalytics(true);
+    }
+  }, [])
+
   return <Component {...pageProps} />;
 }
